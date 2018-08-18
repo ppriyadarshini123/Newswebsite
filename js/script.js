b94e2c5b19686fe815be369abc0461196830380d
Payal <payal.priyadarshini@gmail.com>
2018-08-18 17:56:02 +0100

    updating

---
+++ js/script.js
@@ -0,0 +1,44 @@
+
+
+function showPic1() {
+    document.getElementById("bigPic").style.backgroundImage='url("./img/new_6.jpg")';
+    document.getElementById("p1").innerHTML = "India wins test Cricket against West Indies by 100 runs.";
+   
+     document.getElementById("time").innerHTML = "2 hrs";
+     document.getElementById("place").innerHTML = "England";
+}
+
+function showPic1URL() {
+   
+    window.open("#");
+}
+
+
+function showPic2() {
+   
+    document.getElementById("bigPic").style.backgroundImage='url("./img/new_5.jpg")';
+    
+    document.getElementById("p1").innerHTML = "Kenyan President very keen on resolving the issue of deforestation.";
+    document.getElementById("time").innerHTML = "4 hrs";
+    document.getElementById("place").innerHTML = "Kenya";
+}
+
+function showPic2URL() {
+   
+    window.open("#");
+}
+
+function showPic3() {
+   
+    document.getElementById("bigPic").style.backgroundImage='url("./img/new_4.jpg")';
+    document.getElementById("p1").innerHTML = "The Prime Minister inaugurates the fastest train between Manchester and Edinburgh.";    
+     document.getElementById("time").innerHTML = "3 hrs";
+    document.getElementById("place").innerHTML = "Edinburgh";
+}
+function showPic3URL() {
+   
+    window.open("#");
+}
+
+
+
